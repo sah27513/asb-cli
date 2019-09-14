9ebcaa7d5ab0e5db629eddd6940765eff3980a38
stephen halliburton <sah27513@gmail.com>
2019-09-14 00:45:50 -0400

    Added functionality to connect to ASB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 // ./lib/index.js
+import { config as env } from "dotenv";
+import { MissingConnection } from "src/errors";
+import azure from "azure";
+import chalk from "chalk";
+
+// Configure Environment
+env();
 
 /**
- *
+ * Azure Service Bus Utility
+ * @param {array} args -- Array of Command Line ARGS
  */
 export function asb(args) {
-  console.log(args);
+  // Check if the connection is Defined
+  if (!process.env.AZURE_SERVICEBUS_CONNECTION_STRING) {
+    throw new MissingConnection();
+  }
+
+  // Create an instance of Azure Service Bus
+  const serviceBus = azure.createServiceBusService();
+  console.log(
+    chalk.green("info:"),
+    chalk.grey("Successfully Connected to:"),
+    serviceBus.host
+  );
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,53 @@
-import asb from "asb";
+import * as asb from "asb";
+import { MissingConnection } from "src/errors";
+import { config as env } from "dotenv";
+import azure from "azure";
+import chalk from "chalk";
+
+// Import Mock Data
+import { serviceBus } from "test/data";
+
+// Instantiate Variables
+let main;
 
 describe("Azure Service Bus CLI", () => {
+  beforeEach(() => {
+    main = jest.spyOn(asb, "asb");
+    jest.spyOn(azure, "createServiceBusService").mockReturnValue(serviceBus);
+    jest.spyOn(global.console, "log");
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+
+    // Restore the Environment
+    env();
+  });
   /**
    * General
    */
   test("It Can connect to Azure Service Bus", async next => {
+    // Run the test
+    main();
+
+    // Assertions
+    expect(console.log).toHaveBeenCalledWith(
+      chalk.green("info:"),
+      chalk.grey("Successfully Connected to:"),
+      serviceBus.host
+    );
+
+    // Complete the Test
+    next();
+  });
+
+  test("Throws an error when AZURE_SERVICEBUS_CONNECTION_STRING is empty or null", async next => {
+    // Setup the test
+    delete process.env.AZURE_SERVICEBUS_CONNECTION_STRING;
+
+    // Assertions
+    expect(main).toThrow(new MissingConnection());
+
     // Complete the Test
     next();
   });
